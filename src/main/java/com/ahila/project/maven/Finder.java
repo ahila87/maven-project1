a97ac8bebda4ceb013bd9d05d9773d107ff57c81
ahila muthuswamy <azm0180@tigermail.auburn.edu>
2019-09-03 20:13:06 -0500

    final commit

--- src/main/java/com/ahila/project/maven/Finder.java
+++ src/main/java/com/ahila/project/maven/Finder.java
@@ -3,7 +3,7 @@ package com.ahila.project.maven;
 public class Finder {
 
 	 public static int findMax(int[] intArray){  
-		 System.out.println(intArray);
+		
 		    if(intArray == null || intArray.length == 0) return 0;
 		  
 	        int max = intArray[0];  
@@ -11,20 +11,20 @@ public class Finder {
 	            if(max<intArray[i])  
 	                max=intArray[i];  
 	        }  
-	   	 System.out.println(max);
+	   	 
 	        return max;  
 		    
 	    } 
 	 
 	 public static int findMin(int[] intArray){  
-		 System.out.println(intArray);
+		
 		 if(intArray == null || intArray.length == 0) return 0;
 		 int min = intArray[0];  
 	        for(int i=1;i<intArray.length;i++){  
 	            if(min>intArray[i])  
 	                min=intArray[i];  
 	        }  
-	    	System.out.println(min);
+	    	
 	        return min;  
 	    }  
 
